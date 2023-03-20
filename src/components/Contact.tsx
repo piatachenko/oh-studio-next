6b2c00b85fe1a8172d5347a55588d036fa38f62c
piatachenko <122259487+piatachenko@users.noreply.github.com>
2023-03-20 20:42:38 +0100

    quote mark changed

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,7 +2,7 @@ export default function Contact() {
   return (
     <div className="animate-[load_1s]">
       <div className="flex flex-col items-center justify-center text-[2.375rem] leading-[2.9rem] mt-[12.8rem] mb-[12.25rem]">
-        <div>Let's work together.</div>
+        <div>Let&apos;s work together.</div>
         <div className="text-[#7F7F7F]">
           <a href="mailto:oli@oh.studio">Get in touch.</a>
         </div>
